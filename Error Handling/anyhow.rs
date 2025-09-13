e62f59a3673cd6c8ebc698995132a1059c71516c
Khan.shizaalam <146518802+SheezaAlam@users.noreply.github.com>
2025-09-13 14:04:40 +0500

    Update anyhow.rs

--- Error Handling/anyhow.rs
+++ Error Handling/anyhow.rs
@@ -1,1 +1,19 @@
+use anyhow::Result;
+use std::fs;
 
+fn main() -> Result<()> {
+    let data = fs::read_to_string("hello.txt")?;
+    println!("File contents: {}", data);
+    Ok(())
+}
+//adding extra info - context
+
+
+use anyhow::{Context, Ok, Result};
+use std::fs;
+fn main() -> Result<()>{
+    let data= fs::read_to_string("hi.txt")
+       .context("failed to read")?;
+    println!("contents:{}",data);
+    Ok(())
+}
